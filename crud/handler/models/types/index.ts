b3ced766ca75aa400e8edd4034a67ab012c1692e
haritAegis <133956749+haritAegis@users.noreply.github.com>
2023-05-24 17:40:27 +0530

    Update index.ts

--- crud/handler/models/types/index.ts
+++ crud/handler/models/types/index.ts
@@ -37,21 +37,9 @@ export type MySQLTypes =
 
 // A Record type that has keys of MySQLTypes and values of string
 
-// type key<T> = keyof T;
-
 type keys<T> = {
   [K in keyof T]: T[K] extends Function ? never : K;
 }[keyof T];
 
 export type MySQLRecord<T, U = string> = Record<keys<T>, U>;
-
-// type ValuesAsStringArray<T> = {
-//   [K in keyof T]: T[K] extends string[] ? T[K][number] : never;
-// };
-
-// type ExtractAttributeValues<T> = ValuesAsStringArray<T>[keyof T & 'attributes'];
-
-// export type MySQLRecord<T> = {
-//   [K in ExtractAttributeValues<T> & string]: MySQLTypes;
-// };
 
